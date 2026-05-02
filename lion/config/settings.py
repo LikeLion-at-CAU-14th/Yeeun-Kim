08808d0fd6434f227302962c8c0d5765c46d90d2
김예은 <lpdms12@gmail.com>
2026-05-03 04:03:38 +0900

    feat: API CBV로 수정 및 기능 추가

--- lion/config/settings.py
+++ lion/config/settings.py
@@ -152,6 +152,7 @@ PROJECT_APPS = [
 
 THIRD_PARTY_APPS = [
     "corsheaders",
+    "rest_framework",
 ]
 
 
--- lion/posts/urls.py
+++ lion/posts/urls.py
@@ -3,12 +3,10 @@ from django.urls import path, include
 from posts.views import *
 
 urlpatterns = [
-    #path('', hello_world, name = 'hello_world'),
-    #path('page', index, name='my-page'),
-    #path('<int:id>', get_post_detail)
+    path('', PostList.as_view()), # post 전체 조회
+    path('<int:post_id>/', PostDetail.as_view()), # post 개별 조회
     
-    path('', post_list, name = "post_list"), # Post 생성, 전체 조회
-    path('<int:post_id>/', post_detail, name = "post_detail"), # Post 단일조회, 수정, 삭제
-    path('<int:post_id>/comment/', comment_list, name = "comment_list"), # Post의 Comment 전체 조회
-    path('category/<int:category_id>/', post_list_by_category, name = "post_list_by_category") # Category 별 Post 전체 조회
+    path('<int:post_id>/comment/', CommentList.as_view()), # Post의 Comment 전체 조회
+    path('<int:post_id>/comment/<int:comment_id>/', CommentDetail.as_view()), # comment 개별 삭제
+    path('category/<int:category_id>/', PostListByCategory.as_view()) # Category 별 Post 전체 조회
 ]
