808ff03724271dd5725924b89ee4712da898a3c1
김예은 <lpdms12@gmail.com>
2026-05-30 02:28:06 +0900

    feat: 방명록 base url 배포용

--- lion/config/urls.py
+++ lion/config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
     path('post/', include('posts.urls')),
+    path('guestbook/', include('guestbooks.urls')),
 ]
